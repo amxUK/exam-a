b67453dd6e5e1e7ca6d400666ecce220101242c5
amxUK <118188782+amxUK@users.noreply.github.com>
2023-03-03 13:10:26 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -2,83 +2,133 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#define HORIZ 0
+#define VERT 1
 
+void saisir(int grid[9][9]) {                      //création grille
+    printf("\n");
+    int i, j, v;                                   //i,j,v=lignes,colonnes,nombres
+printf("choisissez le nombre des lignes: \n");
+scanf("%d", &i);
+printf("choisissez le nombre des Colonnes : \n");
+scanf("%d", &j);
+printf("choisissez le nombre de numéro : \n");
+scanf("%d", &v);
+                                                   //on prend le nombre de lignes, colonnes, et valeurs.
+if(i < 0 || i > 8 || j < 0 || j > 8 || v < 1 || v > 9) {
+printf("Erreur\n");                         //nb de ligne  0-8, nb de colonnes 0-8, nb valeurs 1-9 sinon message erreurs
+}
+else {
+if(grid[i][j] == 0) {
+grid[i][j] = v;                                    //i,j=0 - i,j=v
+} else {
+printf("Erreur\n");
+}
+}
+}
+                                                    //ligne colonne valide ?
+int verifierLigneColonne(int grid[9][9], int value, int sens) {
+    int i;
+    int array[9];
+    if(sens == 0) {
+        for(i = 0; i < 9; i++) {
+            array[i] = grid[i][value];
+        }
+    } else if(sens == 1) {
+        for(i = 0; i < 9; i++) {
+            array[i] = grid[value][i];
+        }
+    }
 
-/*
-Pour l'exercice vous aurez besoin de generer des entiers aléatoire : rand() renvoit un entier aléatoire.
-il s'utilise : rand() % NOMBREMAX + 1 
-Pour un entier aléatoire entre 0 et 1 il faut donc faire rand() %2
-voir dans la methode main.
-*/
+    int sum = 0;
+    for(i = 0; i < 9; i++) {
+        sum += array[i];
+    }
 
+    if(sum == 45) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
 
+                                            //combien de cases ont déjà été remplies, utile pour vérifier la validité de la grille
 
+void generer(int grid[9][9]) {
+    int ligne, colonne;
+    int nombrenonnul = 0;
 
+    for (ligne = 0; ligne < 9; ligne++) {
+        for (colonne = 0; colonne < 9; colonne++) {
+            if (grid[ligne][colonne] != 0) {
+                nombrenonnul++;
+            }
+        }
+    }
 
+    for(ligne = 0;ligne < 9; ++ligne) {
+        for(colonne = 0; colonne < 9; ++colonne) printf("%d ", grid[ligne][colonne]); printf("\n");  //grille
+    }
 
-// Ecrire la fonction generer(), elle prend en paramètre la grille et renvoie le nombre d'éléments non nuls
-
-// Ce lien vous sera utile : https://www.geeksforgeeks.org/pass-2d-array-parameter-c/
-
-
-/*Écrire une fonction saisir() qui demande à l’utilisateur de saisir au clavier les indices i et j et la valeur v à placer à l’emplacement (i,j).
-La fonction doit vérifier la validité des indices et de la valeur.
-Si la case n’est pas occupée, la valeur doit être placée dans la grille. remplissage est alors incrémentée*/
-
-
-/*
-Écrire la fonction verifierLigneColonne() qui prend en paramètre un numéro et un sens (HORIZ ou VERT)
-qui vérifie que la ligne ou la colonne (suivant les cas) numéro est bien remplie.
-On pourra utiliser un tableau intermédiaire pour vérifier cela. La fonction retournera 1 si tout s’est bien passé, 0 sinon.
- Les constantes HORIZ de valeur 0 et VERT de valeur 1 sont à définir.
-*/
-
-/*
-Écrire la fonction verifierRegion() qui prend en paramètre deux indices k et l qui correspondent à la région (k,l)
-et qui renvoie 1 si la région est correctement remplie, 0 sinon.
-*/
-
-//Écrire la fonction verifierGrille() qui renvoie 1 si la grille est correctement remplie et 0 sinon
-
-
-//Écrire le programme principal, en supposant que la seule condition d’arrêt est la réussite du sudoku (ce test ne devra être fait que si nécessaire)
-
+    printf("non nuls : %d\n", nombrenonnul);
+}
+//int verifierRegion(int grid[9][9], int k, int l) {
+//    int i, j;
+//    int array[9];
+//    int index = 0;
+//
+//    for(i = 0; i < 3; i++) {
+//        for(j = 0; j < 3; j++) {
+//            array[index] = grid[i + k][j + l];
+//            index++;
+//        }
+//    }
+int verifierGrille(int grid[9][9]) {
+ //   bool region = false;
+ //   bool ligne = false;
+ //   bool colonne = false;
+ //   for (int i = 3; i < 9; i += 3) {
+ //       for(int j = 3; j < 9; j += 3) {
+ //           if (verifierRegion(grid, i, j) == 0) {
+  //              region = true;
+ //           }
+ //       }
+ //   }
 
 int main(){
     // Ne pas toucher le code entre les commentaires
-    srand( time( NULL ) );
-
-
-    int i, j, k;
+    srand(time(NULL));
+    int i, j;
     int solution[9][9];
-    printf("SOLUTION");  
-    printf("\n");  
-    printf("---------------------------------");  
-    printf("\n");  
-    for(j=0;j<9; ++j) 
-    {
-    for(i=0; i<9; ++i)
-        solution[j][i] = (i + j*3 +j /3) %9 +1 ; 
+    printf("SOLUTION");
+    printf("\n");
+    printf("---------------------------------");
+    printf("\n");
+    for(j=0;j<9; ++j) {
+        for(i=0; i<9; ++i) solution[j][i] = (i + j*3 +j /3) % 9 +1 ;
     }
-    
-    for(i=0;i<9; ++i) 
-    {
-    for(j=0; j<9; ++j)
-        printf("%d ", solution[i][j]);
-    printf("\n");  
+    int num_to_remove  = 30;
+    while (num_to_remove > 0) {
+        int row = rand() % 9;
+        int col = rand() % 9;
+        if (solution[row][col] != 0) {
+            solution[row][col] = 0;
+            num_to_remove--;
+        }
     }
-    printf("---------------------------------");  
-    printf("\n");  
-    //toucher le code entre les commentaires 
-    
-    printf("%d",rand() % 2);
-    
-  
+    generer(solution);
+    printf("\n");
+    int valid = 1;
+    while(valid == 1) {
+        saisir(solution);
+        generer(solution);
+        valid = verifierGrille(solution);
+    }
 
-
-
+    printf("reussi\n");
     //Ne pas toucher au code ci dessous
     system("pause");
     return 0;
 }
 
+//pas fini
